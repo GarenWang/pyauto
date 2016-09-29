60206d1dce332af3f2ff0123470535fc8d658885
GarenWang <GarenWang@users.noreply.github.com>
2016-09-29 16:35:49 +0800

    Update simple1.py

--- 第二章/difflib/simple1.py
+++ 第二章/difflib/simple1.py
@@ -20,3 +20,27 @@ text2_lines = text2.splitlines()
 d = difflib.Differ()  #创建Differ对象
 diff = d.compare(text1_lines, text2_lines)  #采用compare方法对字符串进行比较
 print '\n'.join(list(diff)) #在每个列表元素后加上换行符进行格式化输出
+
+
+输出示例：
+shiyanlou:pythontest/ $ python diFF.py                                                                                    [16:24:35]
+- text1:
+?     ^
+
++ text2:
+?     ^
+
+- This module provides classes and functions for comparing sequences.
+?                                                ^
+
++ This module provides classes and functions for Comparing sequences.
+?                                                ^
+
+  including HTML and context and unified diffs.
+- difflib document v7.4
+?                     ^
+
++ difflib document v7.5
+?                     ^
+
+- add string
